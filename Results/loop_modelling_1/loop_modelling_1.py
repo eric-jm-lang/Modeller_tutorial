c96c0e78902672835f4b7f43601d477ed08e9bcc
Eric Lang <el14718@eduroam-244-197.nomadic.bris.ac.uk>
2017-11-27 12:00:27 +0000

    changes output

---
+++ Results/loop_modelling_1/loop_modelling_1.py
@@ -0,0 +1,20 @@
+from modeller import *
+from modeller.automodel import * 
+
+log.verbose()
+env = environ()
+env.io.atom_files_directory = ['.', '../atom_files']
+
+# This is where you will need to modify the script
+a = loopmodel(env, alnfile = 'alignment.ali',
+              knowns = '1qg8', sequence = '1qg8_fill')  
+              
+# First a single model with the missing residue is created
+a.starting_model= 1
+a.ending_model  = 1
+
+# Then the shortest loop is refined and 2 models created
+a.loop.starting_model = 1
+a.loop.ending_model   = 2
+a.loop.md_level       = refine.fast
+a.make()
